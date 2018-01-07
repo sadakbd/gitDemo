a6f3f2d903d2acd5868e750c120647012c8a67f6
sadakbd <sadakbd@gmail.com>
2018-01-07 14:34:41 -0500

    commit to br-1

--- src/main/java/com/example/gitDemo/GitDemoApplication.java
+++ src/main/java/com/example/gitDemo/GitDemoApplication.java
@@ -9,5 +9,7 @@ public class GitDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
 		System.out.println("I am here");
+		System.out.println("This is br-1");
+		
 	}
 }
